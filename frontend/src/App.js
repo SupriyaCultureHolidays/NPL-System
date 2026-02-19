cd0554c60f9df3eca4798099bb1941c223f70492
SupriyaCultureHolidays <supriya.cultureholidays@gmail.com>
2026-02-19 13:01:46 +0530

    creating frontend file and folder structure

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ChatUi from './Components/ChatUi';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>NPL System</h1>
+      <ChatUi />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Components/ChatUi.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ChatUi() {
+    return (
+        <div>ChatUi</div>
+    )
+}
+
+export default ChatUi
